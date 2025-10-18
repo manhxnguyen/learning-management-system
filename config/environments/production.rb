1761c373cf92dd7f4b1e73eb9f186daff223f79c
Manh Nguyen <manhxnguyen11@gmail.com>
2025-10-18 01:43:04 -0500

    update email settings and update seed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: "https://learning-management-system-pyd9.onrender.com" }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
@@ -68,6 +68,15 @@ Rails.application.configure do
   #   port: 587,
   #   authentication: :plain
   # }
+
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.dig(:render, :smtp_username),
+    password: Rails.application.credentials.dig(:render, :smtp_password),
+    address: "smtp.resend.com",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create a default admin user
+if !Admin.exists?(email: "admin@example.com")
+  Admin.create!(email: "admin@example.com", password: "password")
+end
