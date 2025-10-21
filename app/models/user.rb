795961e180cf5f2a547501008987eaf3c9962ce8
Manh Nguyen <manhxnguyen11@gmail.com>
2025-10-20 23:33:44 -0500

    auto confirm when sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  # Auto-confirm user after creation use for deploy to Render
+  after_create :auto_confirm_user
+
   has_many :lesson_users
   has_many :course_users
+
+  private
+
+  def auto_confirm_user
+    self.confirm unless confirmed?
+  end
 end
